b71533db17646eeb0d9dd80d06ba5f1c3685a34b
Ramon Dodd <ramon.dodd@sky.uk>
2015-10-23 12:29:29 +0100

    added selectlist test

--- __tests__/Button-test.js
+++ __tests__/Button-test.js
@@ -1,25 +1,26 @@
-//https://github.com/facebook/jest/blob/master/examples/react-es6/__tests__/CheckboxWithLabel-test.js
-// https://www.npmjs.com/package/react-test-utils
+
 jest.dontMock('../src/public/app/components/Button.jsx');
 import React from 'react/addons';
 const Button = require('../src/public/app/components/Button.jsx');
 var TestUtils = React.addons.TestUtils;
+// http://simonsmith.io/unit-testing-react-components-without-a-dom/
+const shallowRenderer = TestUtils.createRenderer();
 
 describe('Button', () => {
 
-  it('changes the text after click', () => {
+  it('should display correct text and trigger props.onClick()', () => {
 
-  //  let handleSubmit = sinon.spy();
-    let handleSubmit = () => {};
-    // Render a checkbox with label in the document
-    var button = TestUtils.renderIntoDocument(
+    let handleSubmit = jest.genMockFunction();
+
+    var buttonComponent = TestUtils.renderIntoDocument(
       <Button className='btn btn-success' onClick={handleSubmit}>Submit</Button>
     );
 
-    var buttonNode = React.findDOMNode(button);
-
+    var buttonNode = React.findDOMNode(buttonComponent);
     expect(buttonNode.textContent).toEqual('Submit');
 
+    TestUtils.Simulate.click(buttonComponent.refs.button);
+    expect(handleSubmit.mock.calls.length).toBe(1);
   });
 
 });
--- src/public/app/components/Button.jsx
+++ src/public/app/components/Button.jsx
@@ -5,7 +5,7 @@ class Button extends React.Component {
     render() {
         let newClassName = (this.props.className || '') + ' btn btn-lg';
         return (
-            <button className={newClassName} onClick={this.props.onClick}>{this.props.children}</button>
+            <button className={newClassName} onClick={this.props.onClick} ref="button">{this.props.children}</button>
         );
     }
 }
